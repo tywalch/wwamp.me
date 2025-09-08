443ced064fea8cd223b5011f1779215aaf905b41
ty <ty@wattch.io>
2025-09-08 17:02:34 -0400

    Better wrapping

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,5 +11,5 @@ npm run build
 if pm2 list | grep "wwamp.me"; then
   pm2 reload "wwamp.me"
 else
-  pm2 start services/api/dist/bin/www.js --name "wwamp.me"
+  pm2 start "services/api/dist/bin/www.js" --name "wwamp.me"
 fi
