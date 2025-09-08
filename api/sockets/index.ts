23f416908ae7b05972a295ff16cf1d32fa20ff87
ty <ty@wattch.io>
2025-09-08 12:45:39 -0400

    new stuff

--- api/sockets/index.ts
+++ api/sockets/index.ts
@@ -4,8 +4,22 @@ import { Server as IOServer } from 'socket.io';
 export function register(server: Server): IOServer {
     const io = new IOServer(server);
     io.on('connection', (socket) => {
-        socket.on('chat message', (msg) => {
-            io.emit('chat message', msg);
+        // socket.onAny((event, ...args) => {});          
+        socket.on('join', (evt) => {
+            console.log('socket.on join', evt, typeof evt);
+            io.emit('join', JSON.parse(evt));
+        });
+        socket.on('leave', (evt) => {
+            console.log('socket.on leave', evt, typeof evt);
+            io.emit('leave', JSON.parse(evt));
+        });
+        socket.on('sound', (evt) => {
+            console.log('socket.on sound', evt, typeof evt);
+            io.emit('sound', JSON.parse(evt));
+        });
+        socket.on('suppressed', (evt) => {
+            console.log('socket.on suppressed', evt, typeof evt);
+            io.emit('suppressed', JSON.parse(evt));
         });
         return io;
     });
