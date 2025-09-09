6851c3e362a57b3a9693ceef02c2a7ef382cbd33
ty <ty@wattch.io>
2025-09-09 11:22:29 -0400

    Adds cookies for identification

--- services/api/routes/index.ts
+++ services/api/routes/index.ts
@@ -1,26 +1,16 @@
 import express, { type Express, type Router } from 'express';
-
+import { randomUUID as uuid } from 'node:crypto';
 
 function register(app: Express): Router {
   const router = express.Router();
   app.use('/', router);
   
-  router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-  });
-
-  router.get('/sprites', function(req, res, next) {
-    res.json({
-      data: {}
-    });
-
-    router.get('/wwamps', function() {
-      res.json({
-        data: {
-          wwamps: [],
-        },
-      });
-    })
+  router.get('/', function(req, res) {
+    if (!req.cookies?.user_id) {
+      const userId = uuid();
+      res.cookie('user_id', userId, { httpOnly: true, sameSite: 'lax' });
+    }
+    res.render('index', { title: 'WWAMP' });
   });
   
   return router;
--- services/api/sockets/index.ts
+++ services/api/sockets/index.ts
@@ -1,8 +1,34 @@
+import { serialize, parse } from "cookie";
 import { Server } from 'node:http';
+
 import { Server as IOServer } from 'socket.io';
+import { randomUUID as uuid } from 'node:crypto';
 
 export function register(server: Server): IOServer {
     const io = new IOServer(server);
+
+    io.engine.on("initial_headers", (headers) => {
+        const cookies = parse(headers.cookie ?? '');
+        if (!cookies.user_id) {
+            headers["set-cookie"] = serialize("user_id", uuid(), { sameSite: "strict", httpOnly: true });
+        }
+    });
+
+    io.engine.on("headers", (headers, request) => {
+        if (!request.headers.cookie) {
+            return
+        };
+        
+        const cookies = parse(request.headers.cookie);
+        let userId = cookies.user_id;
+
+        console.log('socket.on headers', cookies.user_id);
+
+        request.user = {
+            id: userId
+        };
+    });
+
     io.on('connection', (socket) => {
         // socket.onAny((event, ...args) => {});          
         socket.on('join', (evt) => {
@@ -14,7 +40,6 @@ export function register(server: Server): IOServer {
             io.emit('leave', JSON.parse(evt));
         });
         socket.on('sound', (evt) => {
-            console.log('socket.on sound', evt, typeof evt);
             io.emit('sound', JSON.parse(evt));
         });
         socket.on('suppressed', (evt) => {
