7d8b266f84850583a1f44ec5e431c12c5f45b49f
ty <ty@wattch.io>
2025-09-09 11:22:18 -0400

    - Improves startup scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
 
 npm install
-# if ! command -v pm2 &> /dev/null; then
-#   npm install -g pm2
-# fi
-# if ! command -v typescript &> /dev/null; then
-#   npm install -g typescript
-# fi
 npm run build
 if pm2 list | grep "wwamp.me"; then
   pm2 reload "wwamp.me"
